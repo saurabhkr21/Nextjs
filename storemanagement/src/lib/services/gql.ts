5f74fb6b12333603c6918adcf6b39209b9474ffb
saurabhkr21 <saurabh7221@gmail.com>
2025-08-18 13:50:40 +0530

    solved failed to fetch

--- storemanagement/src/lib/services/gql.ts
+++ storemanagement/src/lib/services/gql.ts
@@ -1,3 +1,3 @@
 import { GraphQLClient } from "graphql-request";
-const gqlClient = new GraphQLClient("http://localhost:3000/api/graphql");
+const gqlClient = new GraphQLClient(`${process.env.NEXT_PUBLIC_API_URL}/api/graphql`);
 export default gqlClient;
